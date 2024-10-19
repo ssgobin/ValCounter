9b011a02c365d1060b78c84bdfc9dae8388b6d73
João Vitor <71790362+ssgobin@users.noreply.github.com>
2024-10-19 12:04:56 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ class Bot(commands.Bot):
 
     def __init__(self):
         self.initial_channels = []  # Lista de canais iniciais
-        super().__init__(token='gno2xk9rdd28u8io39tydoci6eaftj', prefix='val!')
+        super().__init__(token='gno2xk9rdd28u8io39tydoci6eaftj', prefix='!')
         print("Bot inicializado.")  # Log ao inicializar o bot
 
     async def event_ready(self):
